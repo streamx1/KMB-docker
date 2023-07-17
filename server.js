4e4c40f8589276b734a0c734ecf4c3c65c4d5a21
Pavel Karabanovich <karabanovich.pavel@gmail.com>
2023-07-17 12:00:19 +0000

    15-00-17-07-2023

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
  1// Подключаем модуль http
 const http = require('http');
 
-// Создаем сервер  1
+// Создаем сервер  12
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
